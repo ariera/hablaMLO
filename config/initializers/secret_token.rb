ea3b30cdaa4c0691528f8c92e34b0e949ad8c78a
ariera <ariera@gmail.com>
2011-07-29 11:18:39 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HablaMLO::Application.config.secret_token = 'dabcb14200f53e4719c8d5464e41ebe4174bd995208f281537542d34cde4974ed203e80b398fc298cfe6227529d5e03426930b72f0cd69c4bab2102b0657c941'
